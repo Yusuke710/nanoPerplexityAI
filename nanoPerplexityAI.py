0de908261507c87d144444b83a209017044486fb
Yusuke710 <youfu1202mo@gmail.com>
2024-08-31 01:22:49 +1000

    stream completion of AI response. Closer Implementation to PerplexityAI

--- nanoPerplexityAI.py
+++ nanoPerplexityAI.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 import time
+import shutil
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from googlesearch import search
@@ -11,13 +12,14 @@ import backoff
 import openai
 
 # -----------------------------------------------------------------------------
-# Default configuration
+# Default configuration and Prompts
 NUM_SEARCH = 10  # Number of links to parse from Google
 SEARCH_TIME_LIMIT = 3  # Max seconds to request website sources before skipping to the next URL
 TOTAL_TIMEOUT = 6  # Overall timeout for all operations
 MAX_CONTENT = 500  # Number of words to add to LLM context for each search result
 MAX_TOKENS = 1000 # Maximum number of tokens LLM generates
 LLM_MODEL = 'gpt-4o-mini' #'gpt-3.5-turbo' #'gpt-4o'
+
 system_prompt_search = """You are a helpful assistant whose primary goal is to decide if a user's query requires a Google search."""
 search_prompt = """
 Decide if a user's query requires a Google search. You should use Google search for most queries to find the most accurate and updated information. Follow these conditions:
@@ -29,11 +31,13 @@ Decide if a user's query requires a Google search. You should use Google search 
 User Query:
 {query}
 """
+
 system_prompt_answer = """You are a helpful assistant who is expert at answering user's queries"""
 answer_prompt = """Generate a response that is informative and relevant to the user's query
 User Query:
 {query}
 """
+
 system_prompt_cited_answer = """You are a helpful assistant who is expert at answering user's queries based on the cited context."""
 cited_answer_prompt = """
 Provide a relevant, informative response to the user's query using the given context (search results with [citation number](website link) and brief descriptions).
@@ -57,10 +61,6 @@ if not OPENAI_API_KEY:
     raise ValueError("OpenAI API key is not set. Please set the OPENAI_API_KEY environment variable.")
 
 client =openai.OpenAI(api_key=OPENAI_API_KEY)
-
-def get_query():
-    """Prompt the user to enter a query."""
-    return input("Enter your question: ")
 
 def trace_function_factory(start):
     """Create a trace function to timeout request"""
@@ -96,8 +96,12 @@ def parse_google_results(query, num_search=NUM_SEARCH, search_time_limit=SEARCH_
         future_to_url = {executor.submit(fetch_webpage, url, search_time_limit): url for url in urls}
         return {url: page_text for future in as_completed(future_to_url) if (url := future.result()[0]) and (page_text := future.result()[1])}
 
+def save_markdown(content, file_path):
+    with open(file_path, 'a') as file:
+        file.write(content)
+
 @backoff.on_exception(backoff.expo, (openai.RateLimitError, openai.APITimeoutError))
-def llm_check_search(query, msg_history=None, llm_model=LLM_MODEL):
+def llm_check_search(query, file_path, msg_history=None, llm_model=LLM_MODEL):
     """Check if query requires search and execute Google search."""
     prompt = search_prompt.format(query=query)
     msg_history = msg_history or []
@@ -112,14 +116,18 @@ def llm_check_search(query, msg_history=None, llm_model=LLM_MODEL):
     cleaned_response = response.lower().strip()
     if re.fullmatch(r"\bns\b", cleaned_response):
         print("No Google search required.")
+        save_markdown(f"# {query}\n\n## Answer\n", file_path)
         return None
     else:
         print(f"Performing Google search: {cleaned_response}")
         search_dic = parse_google_results(cleaned_response)
+        # Format search result in dic into markdown format
+        search_result_md = "\n".join([f"{number+1}. {link}" for number, link in enumerate(search_dic.keys())])
+        save_markdown(f"# {query}\n\n## Sources\n{search_result_md}\n\n## Answer\n", file_path)
         return search_dic
 
 @backoff.on_exception(backoff.expo, (openai.RateLimitError, openai.APITimeoutError))
-def llm_answer(query, msg_history=None, search_dic=None, llm_model=LLM_MODEL, max_content=MAX_CONTENT, max_tokens=MAX_TOKENS):
+def llm_answer(query, file_path, msg_history=None, search_dic=None, llm_model=LLM_MODEL, max_content=MAX_CONTENT, max_tokens=MAX_TOKENS, debug=False):
     """Build the prompt for the language model including the search results context."""
     if search_dic:
         context_block = "\n".join([f"[{i+1}]({url}): {content[:max_content]}" for i, (url, content) in enumerate(search_dic.items())])
@@ -129,61 +137,67 @@ def llm_answer(query, msg_history=None, search_dic=None, llm_model=LLM_MODEL, ma
         prompt = answer_prompt.format(query=query)
         system_prompt = system_prompt_answer
 
-    """Generate a response using the OpenAI language model."""
+    """Generate a response using the OpenAI language model with stream completion"""
     msg_history = msg_history or []
     new_msg_history = msg_history + [{"role": "user", "content": prompt}]
     response = client.chat.completions.create(
         model=llm_model,
         messages=[{"role": "system", "content": system_prompt}, *new_msg_history],
-        max_tokens=max_tokens
-    ).choices[0].message.content
+        max_tokens=max_tokens,
+        stream=True
+    )
 
-    new_msg_history.append({"role": "assistant", "content": response})
-    return response, new_msg_history
+    print("\n" + "*" * 20 + " LLM START " + "*" * 20)
+    content = []
+    for chunk in response:
+        chunk_content = chunk.choices[0].delta.content
+        if chunk_content:
+            content.append(chunk_content)
+            print(chunk_content, end="")  
+            save_markdown(chunk_content, file_path)
+    
+    print("\n" + "*" * 21 + " LLM END " + "*" * 21 + "\n")
+    # change the line for the next question
+    save_markdown("\n\n", file_path) 
+    new_msg_history = new_msg_history + [{"role": "assistant", "content": ''.join(content)}]
 
-def renumber_citations(response):
-    """Renumber citations in the response to be sequential."""
-    # Find all unique citations in the order of their first appearance
-    citations = list(dict.fromkeys(map(int, re.findall(r'\[\(?(\d+)\)?\]', response))))
-
-    citation_map = {old: new for new, old in enumerate(citations, 1)}
-    for old, new in citation_map.items():
-        response = re.sub(rf'\[\(?{old}\)?\]', f'[{new}]', response)
-    return response, citation_map
-
-def generate_citation_links(citation_map, search_dic):
-    """Generate citation links based on the renumbered response."""
-    cited_links = [f"{new}. {list(search_dic.keys())[old-1]}" for old, new in citation_map.items()]
-    return "\n".join(cited_links)
-
-def save_markdown(query, response, search_dic, file_path):
-    """Renumber citations, then save the query, response, and sources to a markdown file."""
-    links_block = ""
-    if search_dic:
-        response, citation_map = renumber_citations(response)
-        links_block = generate_citation_links(citation_map, search_dic) if citation_map else ""
-    output_content = f"# {query}\n\n## Sources\n{links_block}\n\n## Answer\n{response}\n\n" if search_dic else f"# {query}\n\n## Answer\n{response}\n\n"
-
-    # Open the file with the specified mode ('w' for write or 'a' for append)
-    with open(file_path, 'a') as file:
-        file.write(output_content)
-    print(f"AI response saved into {file_path}")
+    return new_msg_history
 
 def main():
     """Main function to execute the search, generate response, and save to markdown."""
     msg_history = None
-    file_path = None
+    file_path = "playground.md"
+    save_path = None
+
+    # start with an empty file
+    with open(file_path, 'w') as file:
+        pass
 
     while True:
-        query = get_query()
-        file_path = file_path or f"{query}.md"
-
-        search_dic = llm_check_search(query, msg_history)
-        response, msg_history = llm_answer(query, msg_history, search_dic)
-        save_markdown(query, response, search_dic, file_path)
-
-        print("-" * 40)
-        print("Press Ctrl+C to quit")
+        query = input("Enter your question: ")
+        #quit the program
+        if query == "q":
+            break
+        # save the content in another file
+        elif query == "s":
+            if save_path:
+                shutil.copy(file_path, save_path)
+                print(f"AI response saved into {save_path}")
+                # reset the saving mechanism
+                with open(file_path, 'w') as file:
+                    pass
+                save_path = None 
+            else:
+                print("No content is saved")
+                continue
+        # LLM answers
+        else:
+            search_dic = llm_check_search(query, file_path, msg_history)
+            msg_history = llm_answer(query, file_path, msg_history, search_dic)
+            save_path = save_path or f"{query}.md" # ensure saved file has the first query as its name 
+            print(f"AI response recorded into {file_path}")
+        print("-" * 51)
+        print("Enter a key for [s]ave or [q]uit")
 
 if __name__ == "__main__":
     main()
